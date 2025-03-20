38ab1f7d5493bd73e0b86ef81000e736bb8fc6ea
Arturo Mora-Rioja <arturomora@gmail.com>
2025-03-20 08:46:36 +0100

    Traditional function substituted by arrow function

--- js/script.js
+++ js/script.js
@@ -4,13 +4,13 @@ import { queryFilms } from './movies.js';
  * Handling of navigation option visual appearance
  */
 document.querySelectorAll('nav button').forEach((menuOption) => {
-    menuOption.addEventListener('click', function() {
-        queryFilms(this.id);
+    menuOption.addEventListener('click', (e) => {
+        queryFilms(e.target.id);
 
         const selectedMenuOption = document.querySelector('nav button.selected');
         if (selectedMenuOption !== null) {
             selectedMenuOption.classList.remove('selected');
         }
-        this.classList.add('selected');
+        e.target.classList.add('selected');
     });
 });
